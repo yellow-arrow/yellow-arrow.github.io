b3c82a429419259b4295154ad677cc7c41481548
yellow-arrow <pavlova95000@gmail.com>
2016-05-30 16:47:29 +0300

    app files changed

--- APP/js/script.js
+++ APP/js/script.js
@@ -1,20 +1,10 @@
-jQuery(function($) {
-    $(".touchslider").touchSlider({
-    container: this,
-    duration: 350, // the speed of the sliding animation in milliseconds
-    delay: 3000, // initial auto-scrolling delay for each loop
-    margin: 5, // borders size. The margin is set in pixels.
-    mouseTouch: true,
-    namespace: "touchslider",
-    next: ".touchslider-next", // jQuery object for the elements to which a "scroll forwards" action should be bound.
-    pagination: ".touchslider-nav-item",
-    currentClass: "touchslider-nav-item-current", // class name for current pagination item.
-    prev: ".touchslider-prev", // jQuery object for the elements to which a "scroll backwards" action should be bound.
-    scroller: viewport.children(),
-    autoplay: false, // whether to move from image to image automatically
-    viewport: ".touchslider-viewport"
-    });
-});
+var myElement = document.getElementById('cube');
 
-$(".touchslider").data("touchslider").stop(); // stop the slider
-$(".touchslider").data("touchslider").start(); // start the slider
+// create a simple instance
+// by default, it only adds horizontal recognizers
+var mc = new Hammer(myElement);
+
+// listen to events...
+mc.on("panleft panright tap press", function(ev) {
+    myElement.textContent = ev.type +" gesture detected.";
+});
